15c19a106d792d56e6c0c607713fd95747a546b2
Reid Netterville III <reidiiius@gmail.com>
2012-06-04 08:32:38 -0500

    Initial commit

---
+++ site/file/nav.js
@@ -0,0 +1,113 @@
+﻿
+function doc_0() {
+window.location.assign("site/lab.html") }
+function doc_1() {
+window.location.assign("site/page/elements_bg.html") }
+function doc_2() {
+window.location.assign("site/page/dom_bg.html") }
+function doc_3() {
+window.location.assign("site/page/sub_bg.html") }
+function doc_4() {
+window.location.assign("site/page/domocta_bg.html") }
+function doc_5() {
+window.location.assign("site/page/subocta_bg.html") }
+function doc_6() {
+window.location.assign("site/triad/triads.html") }
+function doc_7() {
+window.location.assign("site/ch1/quint.html") }
+function doc_8() {
+window.location.assign("site/ch2/node.html") }
+function doc_9() {
+window.location.assign("site/ch3/triton.html") }
+function doc_10() {
+window.location.assign("site/ch4/ornate.html") }
+function doc_11() {
+window.location.assign("site/page/nat.html") }
+function doc_12() {
+window.location.assign("site/page/tan.html") }
+function doc_13() {
+window.location.assign("site/page/juno.html") }
+function doc_14() {
+window.location.assign("site/page/pluto.html") }
+function doc_15() {
+window.location.assign("site/page/6_moon.html") }
+function doc_16() {
+window.location.assign("site/page/5_merc.html") }
+function doc_17() {
+window.location.assign("site/page/3_venus.html") }
+function doc_18() {
+window.location.assign("site/page/1_chiron.html") }
+function doc_19() {
+window.location.assign("site/page/2_chiron.html") }
+function doc_20() {
+window.location.assign("site/page/2_venus.html") }
+function doc_21() {
+window.location.assign("site/page/6_titan.html") }
+function doc_22() {
+window.location.assign("site/page/5_luna.html") }
+function doc_23() {
+window.location.assign("site/page/6_pal.html") }
+function doc_24() {
+window.location.assign("site/page/5_venus.html") }
+function doc_25() {
+window.location.assign("site/page/6_nep.html") }
+function doc_26() {
+window.location.assign("site/page/5_mars.html") }
+function doc_27() {
+window.location.assign("site/page/56_merc.html") }
+function doc_28() {
+window.location.assign("site/page/56_sun.html") }
+function doc_29() {
+window.location.assign("site/page/234_jup.html") }
+function doc_30() {
+window.location.assign("site/page/127_sat.html") }
+function doc_31() {
+window.location.assign("site/page/35_merc.html") }
+function doc_32() {
+window.location.assign("site/page/16_sun.html") }
+function doc_33() {
+window.location.assign("site/page/2_jup.html") }
+function doc_34() {
+window.location.assign("site/page/2_titan.html") }
+function doc_35() {
+window.location.assign("site/page/2_juno.html") }
+function doc_36() {
+window.location.assign("site/page/2_moon.html") }
+function doc_37() {
+window.location.assign("site/page/26_jup.html") }
+function doc_38() {
+window.location.assign("site/page/25_titan.html") }
+function doc_39() {
+window.location.assign("site/page/26_pal.html") }
+function doc_40() {
+window.location.assign("site/page/25_moon.html") }
+function doc_41() {
+window.location.assign("site/page/35_titan.html") }
+function doc_42() {
+window.location.assign("site/page/16_jup.html") }
+function doc_43() {
+window.location.assign("site/page/36_nep.html") }
+function doc_44() {
+window.location.assign("site/page/15_jup.html") }
+function doc_45() {
+window.location.assign("site/page/26_ceres.html") }
+function doc_46() {
+window.location.assign("site/page/25_sat.html") }
+function doc_47() {
+window.location.assign("site/page/26_pluto.html") }
+function doc_48() {
+window.location.assign("site/page/25_jup.html") }
+function doc_49() {
+window.location.assign("site/page/26_nep.html") }
+function doc_50() {
+window.location.assign("site/page/25_sun.html") }
+function doc_51() {
+window.location.assign("site/vault/raga.html") }
+function doc_52() {
+window.location.assign("site/vault/pg-e.html") }
+function doc_53() {
+window.location.assign("site/vault/pg-k.html") }
+function doc_54() {
+window.location.assign("site/melakarta/index.html") }
+function $Back() {
+window.history.back() }
