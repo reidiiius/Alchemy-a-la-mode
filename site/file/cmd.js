896acd4edaa46199ad91fce225b42151555bc08d
Reid Netterville III <reidiiius@gmail.com>
2012-07-27 23:06:43 -0500

    added pointer

--- site/file/cmd.js
+++ site/file/cmd.js
@@ -1,220 +1,165 @@
 ﻿
 function bgChange(bg) {
- 
-document.body.background=bg; }
+ document.body.background=bg;}
 
 function $elements() {
-
-window.open('page/elements_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=340,height=340'); }
+ window.open('page/elements_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=340,height=340');}
  
 function $dom() {
- 
-window.open('page/dom_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=360,height=530'); }
+ window.open('page/dom_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=360,height=530');}
  
 function $sub() {
- 
-window.open('page/sub_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=350,height=520'); }
+ window.open('page/sub_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=350,height=520');}
  
 function $domocta() {
- 
-window.open('page/domocta_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=356,height=528'); }
+ window.open('page/domocta_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=356,height=528');}
  
 function $subocta() {
- 
-window.open('page/subocta_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=362,height=520'); }
+ window.open('page/subocta_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=362,height=520');}
 
 function $tri() {
-
-window.open('triad/triads.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200'); }
+ window.open('triad/triads.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');}
 
 function $qui() { 
-
-window.open('ch1/quint.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200'); }
+ window.open('ch1/quint.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');}
  
 function $mi6() {
-
-window.open('ch2/node.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200'); }
+ window.open('ch2/node.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');}
  
 function $o() {
-
-window.open('ch3/triton.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200'); }
+ window.open('ch3/triton.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');}
  
 function $aug() {
-
-window.open('ch4/ornate.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200'); }
+ window.open('ch4/ornate.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');}
 
 function $nat() {
- 
-window.open('page/nat.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/nat.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $tan() {
- 
-window.open('page/tan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/tan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
 
 function $juno() {
- 
-window.open('page/juno.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/juno.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $pluto() {
- 
-window.open('page/pluto.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/pluto.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $6_moon() {
- 
-window.open('page/6_moon.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/6_moon.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $5_merc() {
- 
-window.open('page/5_merc.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/5_merc.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $3_venus() {
- 
-window.open('page/3_venus.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/3_venus.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $1_chiron() {
- 
-window.open('page/1_chiron.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/1_chiron.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $2_chiron() {
- 
-window.open('page/2_chiron.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/2_chiron.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $2_venus() {
- 
-window.open('page/2_venus.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/2_venus.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $6_titan() {
- 
-window.open('page/6_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/6_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $5_luna() {
- 
-window.open('page/5_luna.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/5_luna.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
 
 function $6_pal() {
- 
-window.open('page/6_pal.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/6_pal.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $5_venus() {
- 
-window.open('page/5_venus.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/5_venus.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
 
 function $6_nep() {
- 
-window.open('page/6_nep.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/6_nep.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $5_mars() {
- 
-window.open('page/5_mars.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/5_mars.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $56_merc() {
- 
-window.open('page/56_merc.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/56_merc.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $56_sun() {
- 
-window.open('page/56_sun.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/56_sun.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $234_jup() {
- 
-window.open('page/234_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/234_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $127_sat() {
- 
-window.open('page/127_sat.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/127_sat.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $35_merc() {
- 
-window.open('page/35_merc.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/35_merc.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $16_sun() {
- 
-window.open('page/16_sun.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/16_sun.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $2_jup() {
- 
-window.open('page/2_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/2_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $2_titan() {
- 
-window.open('page/2_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/2_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
 
 function $2_juno() {
- 
-window.open('page/2_juno.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/2_juno.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $2_moon() {
- 
-window.open('page/2_moon.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/2_moon.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $26_jup() {
- 
-window.open('page/26_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/26_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $25_titan() {
- 
-window.open('page/25_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/25_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $26_pal() {
- 
-window.open('page/26_pal.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/26_pal.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $25_moon() {
- 
-window.open('page/25_moon.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/25_moon.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
 
 function $35_titan() {
- 
-window.open('page/35_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/35_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $16_jup() {
- 
-window.open('page/16_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/16_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $36_nep() {
- 
-window.open('page/36_nep.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/36_nep.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $15_jup() {
- 
-window.open('page/15_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/15_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $26_ceres() {
- 
-window.open('page/26_ceres.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/26_ceres.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $25_sat() {
- 
-window.open('page/25_sat.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/25_sat.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $26_pluto() {
- 
-window.open('page/26_pluto.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/26_pluto.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $25_jup() {
- 
-window.open('page/25_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/25_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
  
 function $26_nep() {
- 
-window.open('page/26_nep.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }  
+ window.open('page/26_nep.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}  
  
 function $25_sun() {
- 
-window.open('page/25_sun.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400'); }
+ window.open('page/25_sun.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');}
 
 function $list() {
- 
-window.open('vault/raga.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=600,height=450'); }
+ window.open('vault/raga.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=600,height=450');}
  
 function $pg_e() {
- 
-window.open('vault/pg-e.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=600'); }  
+ window.open('vault/pg-e.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=600');}  
  
 function $pg_k() {
- 
-window.open('vault/pg-k.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=1260,height=400'); }
+ window.open('vault/pg-k.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=1260,height=400');}
 
 function $Back() {
-
-window.history.back() }
+ window.history.back();}
