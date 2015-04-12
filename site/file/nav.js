58a6e0c92439ba764a83a270dd8edef433278c45
Reid Netterville III <reidiiius@gmail.com>
2015-04-12 12:11:46 -0500

    150412.0

--- site/file/nav.js
+++ site/file/nav.js
@@ -1,121 +1,168 @@
 ﻿
-function doc_0() {
-  window.location.assign("site/lab.html");
+function $Back() {
+  window.history.back();
 }
+
+function bgChange(bg) {
+ document.body.background=bg;
+}
+
+function doc_0() {
+  window.location.assign("site/index.html");
+}
+
 function doc_1() {
   window.location.assign("site/page/elements_bg.html");
 }
+
 function doc_2() {
   window.location.assign("site/page/dom_bg.html");
 }
+
 function doc_3() {
   window.location.assign("site/page/sub_bg.html");
 }
+
 function doc_4() {
   window.location.assign("site/page/domocta_bg.html");
 }
+
 function doc_5() {
   window.location.assign("site/page/subocta_bg.html");
 }
+
 function doc_6() {
-  window.location.assign("site/triad/triads.html");
+  window.location.assign("site/triad/index.html");
 }
+
 function doc_7() {
-  window.location.assign("site/ch1/quint.html");
+  window.location.assign("site/ch1/index.html");
 }
+
 function doc_8() {
-  window.location.assign("site/ch2/node.html");
+  window.location.assign("site/ch2/index.html");
 }
+
 function doc_9() {
-  window.location.assign("site/ch3/triton.html");
+  window.location.assign("site/ch3/index.html");
 }
+
 function doc_10() {
-  window.location.assign("site/ch4/ornate.html");
+  window.location.assign("site/ch4/index.html");
 }
+
 function doc_11() {
   window.location.assign("site/page/nat.html");
 }
+
 function doc_12() {
   window.location.assign("site/page/tan.html");
 }
+
 function doc_13() {
   window.location.assign("site/page/juno.html");
 }
+
 function doc_14() {
   window.location.assign("site/page/pluto.html");
 }
+
 function doc_15() {
   window.location.assign("site/page/6_moon.html");
 }
+
 function doc_16() {
   window.location.assign("site/page/5_merc.html");
 }
+
 function doc_17() {
   window.location.assign("site/page/3_venus.html");
 }
+
 function doc_18() {
   window.location.assign("site/page/1_chiron.html");
 }
+
 function doc_19() {
   window.location.assign("site/page/2_chiron.html");
 }
+
 function doc_20() {
   window.location.assign("site/page/2_venus.html");
 }
+
 function doc_21() {
   window.location.assign("site/page/6_titan.html");
 }
+
 function doc_22() {
   window.location.assign("site/page/5_luna.html");
 }
+
 function doc_23() {
   window.location.assign("site/page/6_pal.html");
 }
+
 function doc_24() {
   window.location.assign("site/page/5_venus.html");
 }
+
 function doc_25() {
   window.location.assign("site/page/6_nep.html");
 }
+
 function doc_26() {
   window.location.assign("site/page/5_mars.html");
 }
+
 function doc_27() {
   window.location.assign("site/page/56_merc.html");
 }
+
 function doc_28() {
   window.location.assign("site/page/56_sun.html");
 }
+
 function doc_29() {
   window.location.assign("site/page/234_jup.html");
 }
+
 function doc_30() {
   window.location.assign("site/page/127_sat.html");
 }
+
 function doc_31() {
   window.location.assign("site/page/35_merc.html");
 }
+
 function doc_32() {
   window.location.assign("site/page/16_sun.html");
 }
+
 function doc_33() {
   window.location.assign("site/page/2_jup.html");
 }
+
 function doc_34() {
   window.location.assign("site/page/2_titan.html");
 }
+
 function doc_35() {
   window.location.assign("site/page/2_juno.html");
 }
+
 function doc_36() {
   window.location.assign("site/page/2_moon.html");
 }
+
 function doc_37() {
   window.location.assign("site/page/26_jup.html");
 }
+
 function doc_38() {
   window.location.assign("site/page/25_titan.html");
 }
+
 function doc_39() {
   window.location.assign("site/page/26_pal.html");
 }
@@ -125,46 +172,267 @@ function doc_40() {
 function doc_41() {
   window.location.assign("site/page/35_titan.html");
 }
+
 function doc_42() {
   window.location.assign("site/page/16_jup.html");
 }
+
 function doc_43() {
   window.location.assign("site/page/36_nep.html");
 }
+
 function doc_44() {
   window.location.assign("site/page/15_jup.html");
 }
+
 function doc_45() {
   window.location.assign("site/page/26_ceres.html");
 }
+
 function doc_46() {
   window.location.assign("site/page/25_sat.html");
 }
+
 function doc_47() {
   window.location.assign("site/page/26_pluto.html");
 }
+
 function doc_48() {
   window.location.assign("site/page/25_jup.html");
 }
+
 function doc_49() {
   window.location.assign("site/page/26_nep.html");
 }
+
 function doc_50() {
   window.location.assign("site/page/25_sun.html");
 }
+
 function doc_51() {
-  window.location.assign("site/vault/raga.html");
+  window.location.assign("site/glyph/index.html");
 }
+
 function doc_52() {
-  window.location.assign("site/vault/pg-e.html");
+  window.location.assign("site/etude/index.html");
 }
 function doc_53() {
-  window.location.assign("site/vault/pg-k.html");
+  window.location.assign("site/kosmos/index.html");
 }
+
 function doc_54() {
   window.location.assign("site/melakarta/index.html");
 }
-function $Back() {
-  window.history.back();
+
+function $elements() {
+ window.open('page/elements_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=340,height=340');
+}
+
+function $dom() {
+ window.open('page/dom_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=360,height=530');
+}
+
+function $sub() {
+ window.open('page/sub_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=350,height=520');
+}
+
+function $domocta() {
+ window.open('page/domocta_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=356,height=528');
+}
+
+function $subocta() {
+ window.open('page/subocta_bg.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=362,height=520');
+}
+
+function $tri() {
+ window.open('triad/index.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');
+}
+
+function $qui() {
+ window.open('ch1/index.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');
+}
+
+function $mi6() {
+ window.open('ch2/index.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');
+}
+
+function $o() {
+ window.open('ch3/index.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');
+}
+
+function $aug() {
+ window.open('ch4/index.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=200');
+}
+
+function $nat() {
+ window.open('page/nat.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $tan() {
+ window.open('page/tan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $juno() {
+ window.open('page/juno.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $pluto() {
+ window.open('page/pluto.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $6_moon() {
+ window.open('page/6_moon.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $5_merc() {
+ window.open('page/5_merc.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $3_venus() {
+ window.open('page/3_venus.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $1_chiron() {
+ window.open('page/1_chiron.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $2_chiron() {
+ window.open('page/2_chiron.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $2_venus() {
+ window.open('page/2_venus.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $6_titan() {
+ window.open('page/6_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $5_luna() {
+ window.open('page/5_luna.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $6_pal() {
+ window.open('page/6_pal.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $5_venus() {
+ window.open('page/5_venus.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $6_nep() {
+ window.open('page/6_nep.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $5_mars() {
+ window.open('page/5_mars.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $56_merc() {
+ window.open('page/56_merc.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $56_sun() {
+ window.open('page/56_sun.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $234_jup() {
+ window.open('page/234_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $127_sat() {
+ window.open('page/127_sat.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $35_merc() {
+ window.open('page/35_merc.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $16_sun() {
+ window.open('page/16_sun.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $2_jup() {
+ window.open('page/2_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $2_titan() {
+ window.open('page/2_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $2_juno() {
+ window.open('page/2_juno.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $2_moon() {
+ window.open('page/2_moon.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $26_jup() {
+ window.open('page/26_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $25_titan() {
+ window.open('page/25_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $26_pal() {
+ window.open('page/26_pal.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $25_moon() {
+ window.open('page/25_moon.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $35_titan() {
+ window.open('page/35_titan.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $16_jup() {
+ window.open('page/16_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $36_nep() {
+ window.open('page/36_nep.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $15_jup() {
+ window.open('page/15_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $26_ceres() {
+ window.open('page/26_ceres.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $25_sat() {
+ window.open('page/25_sat.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $26_pluto() {
+ window.open('page/26_pluto.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $25_jup() {
+ window.open('page/25_jup.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $26_nep() {
+ window.open('page/26_nep.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $25_sun() {
+ window.open('page/25_sun.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=no,resizable=yes,width=400,height=400');
+}
+
+function $list() {
+ window.open('glyph/index.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=400,height=420');
+}
+
+function $pg_e() {
+ window.open('etude/index.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=800,height=600');
+}
+
+function $pg_k() {
+ window.open('kosmos/index.html','_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,copyhistory=no,scrollbars=yes,resizable=yes,width=1260,height=400');
 }
 
