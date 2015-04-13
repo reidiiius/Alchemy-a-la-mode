d8b9611ada003cfca345f7b20792fa6386ae0161
Reid Netterville III <reidiiius@gmail.com>
2015-04-13 14:30:09 -0500

    150413.4

--- site/scripts/nav.js
+++ site/scripts/nav.js
@@ -3,35 +3,20 @@ function goBack() {
   window.history.back();
 }
 
-function bgChg(bg) {
+function bgChg(qp) {
  document.body.background=bg;
 }
 
-function getDoc(pg) {
+function getDoc(qp) {
   window.location.assign(pg);
 }
 
-function opW(pg) {
+function openWin(qp) {
   window.open(pg,'_blank','resizable=yes,width=400,height=400');
 }
 
-function opWm(pg) {
-  window.open(pg,'_blank','resizable=yes,width=340,height=340');
+function openWinS(qp) {
+  window.open(pg,'_blank','scrollbars=yes,resizable=yes,width=400,height=400');
 }
 
-function opWs(pg) {
-  window.open(pg,'_blank','resizable=yes,width=360,height=530');
-}
-
-function opWr(pg) {
-  window.open(pg,'_blank','scrollbars=yes,resizable=yes,width=400,height=420');
-}
-
-function opWe(pg) {
-  window.open(pg,'_blank','scrollbars=yes,resizable=yes,width=800,height=600');
-}
-
-function opWk(pg) {
-  window.open(pg,'_blank','scrollbars=yes,resizable=yes,width=1260,height=400');
-}
 
