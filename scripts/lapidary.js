69097151c698c4b1912609eded4b17611a179387
Reid Netterville III <reidiiius@gmail.com>
2015-04-15 14:52:21 -0500

    150415.0

---
+++ scripts/lapidary.js
@@ -0,0 +1,83 @@
+
+"use strict";
+
+// setup enviroment
+var comPosition = document.getElementById("seascape");
+var vista = comPosition.getContext("2d");
+var lineColor = "DarkSlateGrey";
+
+// demarcate canvas
+  vista.beginPath();
+  vista.moveTo(0, 0);
+  vista.lineTo(0, 630);
+  vista.lineTo(1120, 630);
+  vista.lineTo(1120, 0);
+  vista.closePath();
+  vista.strokeStyle = "aquamarine";
+  vista.stroke();
+
+// first line
+
+  vista.beginPath();
+  vista.moveTo(214, 116);
+  vista.lineTo(906, 116);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+  vista.beginPath();
+  vista.moveTo(214, 132);
+  vista.lineTo(906, 132);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+  vista.beginPath();
+  vista.moveTo(214, 148);
+  vista.lineTo(906, 148);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+  vista.beginPath();
+  vista.moveTo(214, 164);
+  vista.lineTo(906, 164);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+  vista.beginPath();
+  vista.moveTo(214, 180);
+  vista.lineTo(906, 180);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+// second line
+
+  vista.beginPath();
+  vista.moveTo(214, 316);
+  vista.lineTo(906, 316);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+  vista.beginPath();
+  vista.moveTo(214, 332);
+  vista.lineTo(906, 332);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+  vista.beginPath();
+  vista.moveTo(214, 348);
+  vista.lineTo(906, 348);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+  vista.beginPath();
+  vista.moveTo(214, 364);
+  vista.lineTo(906, 364);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+  vista.beginPath();
+  vista.moveTo(214, 380);
+  vista.lineTo(906, 380);
+  vista.strokeStyle = lineColor;
+  vista.stroke();
+
+
