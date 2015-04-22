68daa94281104f30571c1ab44704b1b140d10395
Reid Netterville III <reidiiius@gmail.com>
2015-04-21 21:51:43 -0500

    150421.0

--- scripts/lapidary.js
+++ scripts/lapidary.js
@@ -1,303 +1,84 @@
 
 "use strict";
 
-window.onload = function() {
-
-// setup environment
-
-var MusicalComposition = document.getElementById("seascape")
-var Vista = MusicalComposition.getContext("2d");
-
-var Staff = {
-
-  lineColor      : "DarkSlateGrey",
-
-  bellClaveStyle : "4.75em monospace",
-  bellClaveColor : "Sienna",
-  bellClaveChar  : "\xBE",
-
-  singleBarStyle : "4em monospace",
-  singleBarColor : "DarkSeaGreen",
-  singleBarChar  : "\u2502",
-
-  doubleBarStyle : "4em monospace",
-  doubleBarColor : "DarkSeaGreen",
-  doubleBarChar  : "\u2551"
-
-};
-
-var Note = {
-
-  headStyle    : "2em monospace",
-  headColor    : "SteelBlue",
-  headDark     : "\u25CF",
-
-  stemStyle    : "4em monospace",
-  stemColor    : "DarkCyan",
-  stemChar     : "\x7C",
-
-  flagStyle    : "3em monospace",
-  flagColor    : "LightSeaGreen",
-  highFlagChar : "\x5C",
-  downFlagChar : "\x2F",
-
-  restStyle    : "2.5em monospace",
-  restColor    : "CadetBlue",
-  restChar     : "\x37",
-
-  acciStyle    : "2.5em monospace",
-  acciColor    : "DarkGoldenrod",
-  flatChar     : "\x62",
-  sharpChar    : "\x23"
-
-};
-
-var Sigil = {
-
-  stellarStyle : "3em monospace",
-  stellarColor : "Olive",
-
-  juno    : "\u26B5",
-  vesta   : "\u26B6",
-  sun     : "\u2609",
-  moon    : "\u263D",
-  mercury : "\u263F",
-  venus   : "\u2640",
-  mars    : "\u2642",
-  jupiter : "\u2643",
-  saturn  : "\u2644",
-  uranus  : "\u2645",
-  neptune : "\u2646",
-  pluto   : "\u2647",
-
-  aries       : "\u2648",
-  taurus      : "\u2649",
-  gemini      : "\u264A",
-  cancer      : "\u264B",
-  leo         : "\u264C",
-  virgo       : "\u264D",
-  libra       : "\u264E",
-  scorpio     : "\u264F",
-  sagittarius : "\u2650",
-  capricorn   : "\u2651",
-  aquarius    : "\u2652",
-  pisces      : "\u2653"
-
-};
-
-// demarcate canvas
-
-  Vista.beginPath();
-  Vista.moveTo(0, 0);
-  Vista.lineTo(0, 630);
-  Vista.lineTo(1120, 630);
-  Vista.lineTo(1120, 0);
-  Vista.closePath();
-  Vista.lineWidth = 2;
-  Vista.strokeStyle = "Wheat";
-  Vista.stroke();
-
-// upper staff
-
-  Vista.lineWidth = 1;
-  Vista.strokeStyle = Staff.lineColor;
-
-  Vista.beginPath();
-  Vista.moveTo(214, 120);
-  Vista.lineTo(906, 120);
-  Vista.stroke();
-
-  Vista.beginPath();
-  Vista.moveTo(214, 135);
-  Vista.lineTo(906, 135);
-  Vista.stroke();
-
-  Vista.beginPath();
-  Vista.moveTo(214, 150);
-  Vista.lineTo(906, 150);
-  Vista.stroke();
-
-  Vista.beginPath();
-  Vista.moveTo(214, 165);
-  Vista.lineTo(906, 165);
-  Vista.stroke();
-
-  Vista.beginPath();
-  Vista.moveTo(214, 180);
-  Vista.lineTo(906, 180);
-  Vista.stroke();
-
-// lower staff
-
-  Vista.beginPath();
-  Vista.moveTo(214, 320);
-  Vista.lineTo(906, 320);
-  Vista.stroke();
-
-  Vista.beginPath();
-  Vista.moveTo(214, 335);
-  Vista.lineTo(906, 335);
-  Vista.stroke();
-
-  Vista.beginPath();
-  Vista.moveTo(214, 350);
-  Vista.lineTo(906, 350);
-  Vista.stroke();
-
-  Vista.beginPath();
-  Vista.moveTo(214, 365);
-  Vista.lineTo(906, 365);
-  Vista.stroke();
-
-  Vista.beginPath();
-  Vista.moveTo(214, 380);
-  Vista.lineTo(906, 380);
-  Vista.stroke();
-
-// upper measure bars
-
-  Vista.font = Staff.singleBarStyle;
-  Vista.fillStyle = Staff.singleBarColor;
-
-  Vista.fillText(Staff.singleBarChar, 200, 166);
-  Vista.fillText(Staff.singleBarChar, 495, 166);
-  Vista.fillText(Staff.doubleBarChar, 888, 166);
-
-// lower measure bars
-
-  Vista.fillText(Staff.singleBarChar, 200, 366);
-  Vista.fillText(Staff.singleBarChar, 495, 366);
-  Vista.fillText(Staff.doubleBarChar, 888, 366);
-
-// upper time signature
-
-  Vista.font = Staff.bellClaveStyle;
-  Vista.fillStyle = Staff.bellClaveColor;
-
-  Vista.fillText(Staff.bellClaveChar, 220, 171);
-
-// lower time signature
-
-  Vista.fillText(Staff.bellClaveChar, 220, 371);
-
-// upper eighth rests
-
-  Vista.font = Note.restStyle;
-  Vista.fillStyle = Note.restColor;
-
-  Vista.fillText(Note.restChar, 685, 165);
-  Vista.fillText(Note.restChar, 777, 165);
-
-// lower eighth rests
-
-  Vista.fillText(Note.restChar, 355, 365);
-  Vista.fillText(Note.restChar, 685, 365);
-  Vista.fillText(Note.restChar, 777, 365);
-
-// upper note beams
-
-  Vista.lineWidth = 4;
-  Vista.strokeStyle = Note.flagColor;
-
-  Vista.beginPath();
-  Vista.moveTo(552, 197);
-  Vista.lineTo(640, 190);
-  Vista.stroke();
-
-// lower note beams
-
-  Vista.beginPath();
-  Vista.moveTo(552, 397);
-  Vista.lineTo(640, 390);
-  Vista.stroke();
-
-// upper note flags
-
-  Vista.font = Note.flagStyle;
-  Vista.fillStyle = Note.flagColor;
-
-  Vista.fillText(Note.downFlagChar, 729, 180);
-  Vista.fillText(Note.downFlagChar, 819, 173);
-
-// lower note flags
-
-  Vista.fillText(Note.highFlagChar, 400, 342);
-  Vista.fillText(Note.downFlagChar, 729, 380);
-  Vista.fillText(Note.downFlagChar, 819, 373);
-
-// upper note stems
-
-  Vista.font = Note.stemStyle;
-  Vista.fillStyle = Note.stemColor;
-
-  Vista.fillText(Note.stemChar, 278, 158);
-  Vista.fillText(Note.stemChar, 363, 152);
-  Vista.fillText(Note.stemChar, 448, 144);
-  Vista.fillText(Note.stemChar, 538, 187);
-  Vista.fillText(Note.stemChar, 628, 180);
-  Vista.fillText(Note.stemChar, 718, 173);
-  Vista.fillText(Note.stemChar, 808, 166);
-
-// lower note stems
-
-  Vista.fillText(Note.stemChar, 278, 358);
-  Vista.fillText(Note.stemChar, 390, 352);
-  Vista.fillText(Note.stemChar, 448, 344);
-  Vista.fillText(Note.stemChar, 538, 387);
-  Vista.fillText(Note.stemChar, 628, 380);
-  Vista.fillText(Note.stemChar, 718, 373);
-  Vista.fillText(Note.stemChar, 808, 366);
-
-// upper note heads
-
-  Vista.font = Note.headStyle;
-  Vista.fillStyle = Note.headColor;
-
-  Vista.fillText(Note.headDark, 290, 179);
-  Vista.fillText(Note.headDark, 375, 172);
-  Vista.fillText(Note.headDark, 460, 164);
-  Vista.fillText(Note.headDark, 550, 156);
-  Vista.fillText(Note.headDark, 640, 148);
-  Vista.fillText(Note.headDark, 730, 141);
-  Vista.fillText(Note.headDark, 820, 134);
-
-// lower note heads
-
-  Vista.fillText(Note.headDark, 290, 379);
-  Vista.fillText(Note.headDark, 402, 372);
-  Vista.fillText(Note.headDark, 460, 364);
-  Vista.fillText(Note.headDark, 550, 356);
-  Vista.fillText(Note.headDark, 640, 348);
-  Vista.fillText(Note.headDark, 730, 341);
-  Vista.fillText(Note.headDark, 820, 334);
-
-  Vista.font = Note.acciStyle;
-  Vista.fillStyle = Note.acciColor;
-
-  Vista.fillText(Note.sharpChar, 375, 376);
-
-// upper sigils
-
-  Vista.font = Sigil.stellarStyle;
-  Vista.fillStyle = Sigil.stellarColor;
-
-  Vista.fillText(Sigil.saturn + Sigil.cancer, 275, 85);
-  Vista.fillText(Sigil.sun + Sigil.taurus, 360, 85);
-  Vista.fillText(Sigil.moon + Sigil.pisces, 445, 85);
-  Vista.fillText(Sigil.mars + Sigil.capricorn, 535, 85);
-  Vista.fillText(Sigil.mercury + Sigil.sagittarius, 625, 85);
-  Vista.fillText(Sigil.jupiter + Sigil.libra, 715, 85);
-  Vista.fillText(Sigil.venus + Sigil.leo, 805, 85);
-
-// lower sigils
-
-  Vista.fillText(Sigil.neptune + Sigil.cancer, 275, 288);
-  Vista.fillText(Sigil.mars + Sigil.aries, 380, 288);
-  Vista.fillText(Sigil.mercury + Sigil.pisces, 445, 288);
-  Vista.fillText(Sigil.jupiter + Sigil.capricorn, 535, 288);
-  Vista.fillText(Sigil.uranus + Sigil.sagittarius, 625, 288);
-  Vista.fillText(Sigil.saturn + Sigil.libra, 715, 288);
-  Vista.fillText(Sigil.sun + Sigil.leo, 805, 288);
+var Tranquility_Base = {
+
+    Arrangment: {
+
+      lineColor      : "DarkSlateGrey",
+
+      bellClaveStyle : "4.75em monospace",
+      bellClaveColor : "Sienna",
+      bellClaveChar  : "\xBE",
+
+      singleBarStyle : "4em monospace",
+      singleBarColor : "DarkSeaGreen",
+      singleBarChar  : "\u2502",
+
+      doubleBarStyle : "4em monospace",
+      doubleBarColor : "DarkSeaGreen",
+      doubleBarChar  : "\u2551"
+
+    },
+
+    Orchestration: {
+
+      headStyle    : "2em monospace",
+      headColor    : "SteelBlue",
+      headDark     : "\u25CF",
+
+      stemStyle    : "4em monospace",
+      stemColor    : "DarkCyan",
+      stemChar     : "\x7C",
+
+      flagStyle    : "3em monospace",
+      flagColor    : "LightSeaGreen",
+      highFlagChar : "\x5C",
+      downFlagChar : "\x2F",
+
+      restStyle    : "2.5em monospace",
+      restColor    : "CadetBlue",
+      restChar     : "\x37",
+
+      acciStyle    : "2.5em monospace",
+      acciColor    : "DarkGoldenrod",
+      flatChar     : "\x62",
+      sharpChar    : "\x23"
+
+    },
+
+    Constellation: {
+
+      stellarStyle : "3em monospace",
+      stellarColor : "Olive",
+
+      juno    : "\u26B5",
+      vesta   : "\u26B6",
+      sun     : "\u2609",
+      moon    : "\u263D",
+      mercury : "\u263F",
+      venus   : "\u2640",
+      mars    : "\u2642",
+      jupiter : "\u2643",
+      saturn  : "\u2644",
+      uranus  : "\u2645",
+      neptune : "\u2646",
+      pluto   : "\u2647",
+
+      aries       : "\u2648",
+      taurus      : "\u2649",
+      gemini      : "\u264A",
+      cancer      : "\u264B",
+      leo         : "\u264C",
+      virgo       : "\u264D",
+      libra       : "\u264E",
+      scorpio     : "\u264F",
+      sagittarius : "\u2650",
+      capricorn   : "\u2651",
+      aquarius    : "\u2652",
+      pisces      : "\u2653"
+
+    }
 
 };
 
---
+++ scripts/triton.js
@@ -0,0 +1,230 @@
+
+"use strict";
+
+function kosalam() {
+
+// setup environment
+
+var MusicalComposition = document.getElementById("seascape")
+var Vega = MusicalComposition.getContext("2d");
+
+var Staff = Object.create(Tranquility_Base.Arrangment);
+var Note  = Object.create(Tranquility_Base.Orchestration);
+var Sigil = Object.create(Tranquility_Base.Constellation);
+
+// demarcate canvas
+
+  Vega.beginPath();
+  Vega.moveTo(0, 0);
+  Vega.lineTo(0, 630);
+  Vega.lineTo(1120, 630);
+  Vega.lineTo(1120, 0);
+  Vega.closePath();
+  Vega.lineWidth = 2;
+  Vega.strokeStyle = "Wheat";
+  Vega.stroke();
+
+// upper staff
+
+  Vega.lineWidth = 1;
+  Vega.strokeStyle = Staff.lineColor;
+
+  Vega.beginPath();
+  Vega.moveTo(214, 120);
+  Vega.lineTo(906, 120);
+  Vega.stroke();
+
+  Vega.beginPath();
+  Vega.moveTo(214, 135);
+  Vega.lineTo(906, 135);
+  Vega.stroke();
+
+  Vega.beginPath();
+  Vega.moveTo(214, 150);
+  Vega.lineTo(906, 150);
+  Vega.stroke();
+
+  Vega.beginPath();
+  Vega.moveTo(214, 165);
+  Vega.lineTo(906, 165);
+  Vega.stroke();
+
+  Vega.beginPath();
+  Vega.moveTo(214, 180);
+  Vega.lineTo(906, 180);
+  Vega.stroke();
+
+// lower staff
+
+  Vega.beginPath();
+  Vega.moveTo(214, 320);
+  Vega.lineTo(906, 320);
+  Vega.stroke();
+
+  Vega.beginPath();
+  Vega.moveTo(214, 335);
+  Vega.lineTo(906, 335);
+  Vega.stroke();
+
+  Vega.beginPath();
+  Vega.moveTo(214, 350);
+  Vega.lineTo(906, 350);
+  Vega.stroke();
+
+  Vega.beginPath();
+  Vega.moveTo(214, 365);
+  Vega.lineTo(906, 365);
+  Vega.stroke();
+
+  Vega.beginPath();
+  Vega.moveTo(214, 380);
+  Vega.lineTo(906, 380);
+  Vega.stroke();
+
+// upper measure bars
+
+  Vega.font = Staff.singleBarStyle;
+  Vega.fillStyle = Staff.singleBarColor;
+
+  Vega.fillText(Staff.singleBarChar, 200, 166);
+  Vega.fillText(Staff.singleBarChar, 495, 166);
+  Vega.fillText(Staff.doubleBarChar, 888, 166);
+
+// lower measure bars
+
+  Vega.fillText(Staff.singleBarChar, 200, 366);
+  Vega.fillText(Staff.singleBarChar, 495, 366);
+  Vega.fillText(Staff.doubleBarChar, 888, 366);
+
+// upper time signature
+
+  Vega.font = Staff.bellClaveStyle;
+  Vega.fillStyle = Staff.bellClaveColor;
+
+  Vega.fillText(Staff.bellClaveChar, 220, 171);
+
+// lower time signature
+
+  Vega.fillText(Staff.bellClaveChar, 220, 371);
+
+// upper eighth rests
+
+  Vega.font = Note.restStyle;
+  Vega.fillStyle = Note.restColor;
+
+  Vega.fillText(Note.restChar, 685, 165);
+  Vega.fillText(Note.restChar, 777, 165);
+
+// lower eighth rests
+
+  Vega.fillText(Note.restChar, 355, 365);
+  Vega.fillText(Note.restChar, 685, 365);
+  Vega.fillText(Note.restChar, 777, 365);
+
+// upper note beams
+
+  Vega.lineWidth = 4;
+  Vega.strokeStyle = Note.flagColor;
+
+  Vega.beginPath();
+  Vega.moveTo(552, 197);
+  Vega.lineTo(640, 190);
+  Vega.stroke();
+
+// lower note beams
+
+  Vega.beginPath();
+  Vega.moveTo(552, 397);
+  Vega.lineTo(640, 390);
+  Vega.stroke();
+
+// upper note flags
+
+  Vega.font = Note.flagStyle;
+  Vega.fillStyle = Note.flagColor;
+
+  Vega.fillText(Note.downFlagChar, 729, 180);
+  Vega.fillText(Note.downFlagChar, 819, 173);
+
+// lower note flags
+
+  Vega.fillText(Note.highFlagChar, 400, 342);
+  Vega.fillText(Note.downFlagChar, 729, 380);
+  Vega.fillText(Note.downFlagChar, 819, 373);
+
+// upper note stems
+
+  Vega.font = Note.stemStyle;
+  Vega.fillStyle = Note.stemColor;
+
+  Vega.fillText(Note.stemChar, 278, 158);
+  Vega.fillText(Note.stemChar, 363, 152);
+  Vega.fillText(Note.stemChar, 448, 144);
+  Vega.fillText(Note.stemChar, 538, 187);
+  Vega.fillText(Note.stemChar, 628, 180);
+  Vega.fillText(Note.stemChar, 718, 173);
+  Vega.fillText(Note.stemChar, 808, 166);
+
+// lower note stems
+
+  Vega.fillText(Note.stemChar, 278, 358);
+  Vega.fillText(Note.stemChar, 390, 352);
+  Vega.fillText(Note.stemChar, 448, 344);
+  Vega.fillText(Note.stemChar, 538, 387);
+  Vega.fillText(Note.stemChar, 628, 380);
+  Vega.fillText(Note.stemChar, 718, 373);
+  Vega.fillText(Note.stemChar, 808, 366);
+
+// upper note heads
+
+  Vega.font = Note.headStyle;
+  Vega.fillStyle = Note.headColor;
+
+  Vega.fillText(Note.headDark, 290, 179);
+  Vega.fillText(Note.headDark, 375, 172);
+  Vega.fillText(Note.headDark, 460, 164);
+  Vega.fillText(Note.headDark, 550, 156);
+  Vega.fillText(Note.headDark, 640, 148);
+  Vega.fillText(Note.headDark, 730, 141);
+  Vega.fillText(Note.headDark, 820, 134);
+
+// lower note heads
+
+  Vega.fillText(Note.headDark, 290, 379);
+  Vega.fillText(Note.headDark, 402, 372);
+  Vega.fillText(Note.headDark, 460, 364);
+  Vega.fillText(Note.headDark, 550, 356);
+  Vega.fillText(Note.headDark, 640, 348);
+  Vega.fillText(Note.headDark, 730, 341);
+  Vega.fillText(Note.headDark, 820, 334);
+
+  Vega.font = Note.acciStyle;
+  Vega.fillStyle = Note.acciColor;
+
+  Vega.fillText(Note.sharpChar, 375, 376);
+
+// upper sigils
+
+  Vega.font = Sigil.stellarStyle;
+  Vega.fillStyle = Sigil.stellarColor;
+
+  Vega.fillText(Sigil.saturn + Sigil.cancer, 275, 85);
+  Vega.fillText(Sigil.sun + Sigil.taurus, 360, 85);
+  Vega.fillText(Sigil.moon + Sigil.pisces, 445, 85);
+  Vega.fillText(Sigil.mars + Sigil.capricorn, 535, 85);
+  Vega.fillText(Sigil.mercury + Sigil.sagittarius, 625, 85);
+  Vega.fillText(Sigil.jupiter + Sigil.libra, 715, 85);
+  Vega.fillText(Sigil.venus + Sigil.leo, 805, 85);
+
+// lower sigils
+
+  Vega.fillText(Sigil.neptune + Sigil.cancer, 275, 288);
+  Vega.fillText(Sigil.mars + Sigil.aries, 380, 288);
+  Vega.fillText(Sigil.mercury + Sigil.pisces, 445, 288);
+  Vega.fillText(Sigil.jupiter + Sigil.capricorn, 535, 288);
+  Vega.fillText(Sigil.uranus + Sigil.sagittarius, 625, 288);
+  Vega.fillText(Sigil.saturn + Sigil.libra, 715, 288);
+  Vega.fillText(Sigil.sun + Sigil.leo, 805, 288);
+
+}
+
