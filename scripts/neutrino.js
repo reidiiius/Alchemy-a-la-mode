519018035b81c1145860738cf090a0fe0abd2edc
Reid Netterville III <reidiiius@gmail.com>
2015-08-27 00:15:02 -0500

    150827.0

--- scripts/neutrino.js
+++ scripts/neutrino.js
@@ -1,8 +1,42 @@
 
 function fetchDefs(y, z) {
+  var s = false;
   var x = new XMLHttpRequest();
-     x.open("GET", y, 0);
+  var d = document.getElementById(z);
+
+  if (s) {
+    x.onreadystatechange = function() {
+      if (x.readyState == 4 && x.status == 200) {
+        d.innerHTML = x.responseText;
+      }
+      else {
+        try {
+          if (x.status == 404)
+            throw "File not found - " + y
+          if (x.status == 418)
+            throw "I\'m a teapot"
+          if (x.status == 500)
+            throw "Internal Server Error";
+        }
+        catch(err) {
+          d.innerHTML = err;
+        }
+      }
+    }
+    x.open("GET", y, s);
     x.send();
-  document.getElementById(z).innerHTML = x.responseText;
+  }
+  else {
+    x.open("GET", y, s);
+    x.send();
+    d.innerHTML = x.responseText;
+  }
+}
+
+window.onload = function() {
+  fetchDefs("images\/syzygy.xml", "symBase");
+  fetchDefs("images\/bass\/q4t_n0.xml", "q4t_n0");
+  fetchDefs("images\/cello\/q5t_n0.xml", "q5t_n0");
+  fetchDefs("images\/guitar\/gst_n0.xml", "gst_n0");
 }
 
